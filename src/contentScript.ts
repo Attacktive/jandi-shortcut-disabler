9b1141d1649e8b1835864ea6e1d146167fcf8a07
Attacktive <welcome.hell.dude@gmail.com>
2022-09-16 00:48:50 +0900

    Try implementing

---
+++ src/contentScript.ts
@@ -0,0 +1,8 @@
+const script = document.createElement("script");
+script.setAttribute("type", "text/javascript");
+script.setAttribute("async", "true");
+// @ts-ignore
+script.setAttribute("src", chrome.runtime.getURL("inject.js"));
+
+const head = (document.head || document.getElementsByTagName("head")[0] || document.documentElement);
+head.insertBefore(script, head.firstChild);
---
+++ webpack/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+	mode: "production",
+	entry: {
+		contentScript: path.resolve(__dirname, "..", "src", "contentScript.ts"),
+		inject: path.resolve(__dirname, "..", "src", "inject.ts")
+	},
+	output: {
+		path: path.join(__dirname, "../dist"),
+		filename: "[name].js"
+	},
+	resolve: {
+		extensions: [".ts", ".js"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: "ts-loader",
+				exclude: /node_modules/
+			}
+		]
+	},
+	optimization: {
+		minimize: false
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [{ from: ".", to: ".", context: "public" }]
+		})
+	]
+};
