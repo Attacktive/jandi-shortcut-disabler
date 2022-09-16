79cfd53b1308f3b24afa7e170f8d649cda992db2
Pantera <w.jung@okestro.com>
2022-09-16 13:35:21 +0900

    Refactor code

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,8 +1,6 @@
 const script = document.createElement("script");
 script.setAttribute("type", "text/javascript");
 script.setAttribute("async", "true");
-// @ts-ignore
 script.setAttribute("src", chrome.runtime.getURL("inject.js"));
 
-const head = (document.head || document.getElementsByTagName("head")[0] || document.documentElement);
-head.appendChild(script);
+document.head.appendChild(script);
