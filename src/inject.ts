a654164f20689171be099c3bfa1558334582f979
Attacktive <tribute2pantera@gmail.com>
2023-04-30 04:13:06 +0900

    refactor: replace `@ts-ignore` suppression with slightly better alternative

--- src/inject.ts
+++ src/inject.ts
@@ -11,8 +11,11 @@ type EventHandlerContainer = {
 	type: string
 };
 
-// @ts-ignore: _data is an internal data structure which is undocumented. https://blog.jquery.com/2011/11/08/building-a-slimmer-jquery/
-$._data(window, "events")
+/*
+ * `_data` is an internal function which is undocumented.
+ * https://blog.jquery.com/2011/11/08/building-a-slimmer-jquery/
+ */
+($ as any)._data(window, "events")
 	?.keydown
 	?.forEach((eventListener: EventHandlerContainer) => {
 		$(window).off("keydown", eventListener.handler);
