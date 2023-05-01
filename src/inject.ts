2d42b486f1fb48c87c8e7f8f00474bf945fa8fdd
Attacktive <tribute2pantera@gmail.com>
2023-05-01 14:54:06 +0900

    refactor: go back with `@ts-ignore`

--- src/inject.ts
+++ src/inject.ts
@@ -15,7 +15,8 @@ interface EventHandlerContainer {
  * `_data` is an internal function which is undocumented.
  * https://blog.jquery.com/2011/11/08/building-a-slimmer-jquery/
  */
-($ as any)._data(window, "events")
+// @ts-ignore
+$._data(window, "events")
 	?.keydown
 	?.forEach((eventListener: EventHandlerContainer) => {
 		$(window).off("keydown", eventListener.handler);
