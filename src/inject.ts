6f3b795935c25b4ff5267f81d577dcb9f6a6a158
Attacktive <tribute2pantera@gmail.com>
2023-12-23 15:37:10 +0900

    feature: add logging

--- src/inject.ts
+++ src/inject.ts
@@ -1,12 +1,26 @@
 import EventHandler = JQuery.EventHandler;
 
-interface EventHandlerContainer {
-	handler: EventHandler<Window>;
-}
-
 /*
  * `_data` is an internal function which is undocumented.
  * https://blog.jquery.com/2011/11/08/building-a-slimmer-jquery/
  */
 // @ts-ignore
-$._data(window, "events")?.keydown?.forEach((eventListener: EventHandlerContainer) => $(window).off("keydown", eventListener.handler));
+const keydownEventsOnWindow = $._data(window, "events")?.keydown;
+if (keydownEventsOnWindow) {
+	keydownEventsOnWindow.forEach(({ handler }: { handler: EventHandler<Window> }) => {
+		console.debug(`About to remove keydown event listener: ${handler}`);
+		$(window).off("keydown", handler);
+	})
+} else {
+	console.debug("Seems like they have removed the evil keydown event listeners at all. 😺");
+}
+
+// @ts-ignore
+const keydownEventsOnDocument = $._data(document, "events")?.keydown;
+if (keydownEventsOnDocument) {
+	console.debug("There's some keydown event listener(s) registered on the document which is OK for now:");
+
+	keydownEventsOnDocument.forEach(({ handler }: { handler: EventHandler<Window> }, index: number) => {
+		console.debug(`#${index + 1}`, handler);
+	});
+}
