50aca4b45c47320b4f3bd3e1f72bbaccce029573
Attacktive <tribute2pantera@gmail.com>
2023-12-23 15:43:49 +0900

    refactor: reformat `inject.ts`

--- src/inject.ts
+++ src/inject.ts
@@ -1,10 +1,7 @@
 import EventHandler = JQuery.EventHandler;
 
-/*
- * `_data` is an internal function which is undocumented.
- * https://blog.jquery.com/2011/11/08/building-a-slimmer-jquery/
- */
-// @ts-ignore
+// @ts-expect-error: `_data` is an internal function which is undocumented.
+// https://blog.jquery.com/2011/11/08/building-a-slimmer-jquery/
 const keydownEventsOnWindow = $._data(window, "events")?.keydown;
 if (keydownEventsOnWindow) {
 	keydownEventsOnWindow.forEach(({ handler }: { handler: EventHandler<Window> }) => {
@@ -15,12 +12,10 @@ if (keydownEventsOnWindow) {
 	console.debug("Seems like they have removed the evil keydown event listeners at all. 😺");
 }
 
-// @ts-ignore
+// @ts-expect-error: `_data` is an internal function which is undocumented.
 const keydownEventsOnDocument = $._data(document, "events")?.keydown;
 if (keydownEventsOnDocument) {
 	console.debug("There's some keydown event listener(s) registered on the document which is OK for now:");
 
-	keydownEventsOnDocument.forEach(({ handler }: { handler: EventHandler<Window> }, index: number) => {
-		console.debug(`#${index + 1}`, handler);
-	});
+	keydownEventsOnDocument.forEach(({ handler }: { handler: EventHandler<Window> }, index: number) => console.debug(`#${index + 1}`, handler));
 }
