067d53ca8bb1f8dded2e8f8a0bd556965c55ca64
Attacktive <tribute2pantera@gmail.com>
2024-02-27 14:12:19 +0900

    refactor: remove Eslint suppression

--- src/inject.ts
+++ src/inject.ts
@@ -1,10 +1,15 @@
-import EventHandler = JQuery.EventHandler;
+type EventHandler = JQuery.EventHandler<Window>;
 
-// @ts-expect-error: `_data` is an internal function which is undocumented.
+interface JQueryExtension extends JQueryStatic {
+	_data(element: Window | Document, key: string): any;
+}
+
+const jQueryExtension = ($ as JQueryExtension);
+
 // https://blog.jquery.com/2011/11/08/building-a-slimmer-jquery/
-const keydownEventsOnWindow = $._data(window, "events")?.keydown;
+const keydownEventsOnWindow = jQueryExtension._data(window, "events")?.keydown;
 if (keydownEventsOnWindow) {
-	keydownEventsOnWindow.forEach(({ handler }: { handler: EventHandler<Window> }) => {
+	keydownEventsOnWindow.forEach(({ handler }: { handler: EventHandler }) => {
 		console.debug(`About to remove keydown event listener: ${handler}`);
 		$(window).off("keydown", handler);
 	})
@@ -12,10 +17,9 @@ if (keydownEventsOnWindow) {
 	console.debug("Seems like they have removed the evil keydown event listeners at all. 😺");
 }
 
-// @ts-expect-error: `_data` is an internal function which is undocumented.
-const keydownEventsOnDocument = $._data(document, "events")?.keydown;
+const keydownEventsOnDocument = jQueryExtension._data(document, "events")?.keydown;
 if (keydownEventsOnDocument) {
 	console.debug("There's some keydown event listener(s) registered on the document which is OK for now:");
 
-	keydownEventsOnDocument.forEach(({ handler }: { handler: EventHandler<Window> }, index: number) => console.debug(`#${index + 1}`, handler));
+	keydownEventsOnDocument.forEach(({ handler }: { handler: EventHandler }, index: number) => console.debug(`#${index + 1}`, handler));
 }
