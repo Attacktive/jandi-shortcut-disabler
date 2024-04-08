dad907fabebc08702e535be2bc7c467d4f1c0a86
Attacktive <tribute2pantera@gmail.com>
2024-04-08 13:42:57 +0900

    fix: correct URL to inject.js
    https://github.com/Attacktive/jandi-shortcut-disabler/issues/166

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,6 +1,6 @@
 const script = document.createElement("script");
 script.setAttribute("type", "text/javascript");
 script.setAttribute("async", "true");
-script.setAttribute("src", chrome.runtime.getURL("inject.js"));
+script.setAttribute("src", chrome.runtime.getURL("js/inject.js"));
 
 document.head.appendChild(script);
