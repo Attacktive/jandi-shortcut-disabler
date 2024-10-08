eb0131abf6c6002147d730172a4ecf49c82fd8b1
Attacktive <tribute2pantera@gmail.com>
2024-10-08 10:58:16 +0900

    feature: add support for Firefox
    https://github.com/Attacktive/jandi-shortcut-disabler/issues/255

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,6 +1,8 @@
+const platform = chrome ?? browser;
+
 const script = document.createElement("script");
 script.setAttribute("type", "text/javascript");
 script.setAttribute("async", "true");
-script.setAttribute("src", chrome.runtime.getURL("js/inject.js"));
+script.setAttribute("src", platform.runtime.getURL("js/inject.js"));
 
 document.head.appendChild(script);
